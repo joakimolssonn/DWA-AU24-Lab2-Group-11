ed3a627256ccee3422e89fe77d5f2f38d67f6c05
Joakim Olsson <165900249+joakimolssonn@users.noreply.github.com>
2024-10-21 14:18:16 +0200

    Add project files.

---
+++ DWA-AU24-Lab2-Group-11/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DWA_AU24_Lab2_Group_11.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
