87657343138e8e264d759704f0f2fa01ff4b5754
Abdriano <s2305430@student.hb.se>
2024-11-22 10:31:59 +0100

    Implemented a method for calculating OptimalPlantingDate with PlantingSchedueleService.
    Created a WeatherData class for API

---
+++ DWA-AU24-Lab2-Group-11/Migrations/20241122090146_WeatherDataCreate.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DWA_AU24_Lab2_Group_11.Migrations
+{
+    /// <inheritdoc />
+    public partial class WeatherDataCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "GrowthHistory",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "HarvestDate", "PlantingDate" },
+                values: new object[] { new DateTime(2024, 12, 2, 10, 1, 45, 576, DateTimeKind.Local).AddTicks(9208), new DateTime(2024, 11, 22, 10, 1, 45, 576, DateTimeKind.Local).AddTicks(9204) });
+
+            migrationBuilder.UpdateData(
+                table: "GrowthHistory",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "HarvestDate", "PlantingDate" },
+                values: new object[] { new DateTime(2024, 12, 2, 10, 1, 45, 576, DateTimeKind.Local).AddTicks(9214), new DateTime(2024, 11, 22, 10, 1, 45, 576, DateTimeKind.Local).AddTicks(9211) });
+
+            migrationBuilder.UpdateData(
+                table: "HarvestTracking",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "HarvestDate",
+                value: new DateTime(2024, 12, 2, 10, 1, 45, 576, DateTimeKind.Local).AddTicks(9229));
+
+            migrationBuilder.UpdateData(
+                table: "HarvestTracking",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "HarvestDate",
+                value: new DateTime(2024, 12, 2, 10, 1, 45, 576, DateTimeKind.Local).AddTicks(9234));
+
+            migrationBuilder.UpdateData(
+                table: "PlantingSchedule",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PlantingDate",
+                value: new DateTime(2024, 10, 23, 10, 1, 45, 576, DateTimeKind.Local).AddTicks(9136));
+
+            migrationBuilder.UpdateData(
+                table: "PlantingSchedule",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PlantingDate",
+                value: new DateTime(2024, 9, 23, 10, 1, 45, 576, DateTimeKind.Local).AddTicks(9184));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "GrowthHistory",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "HarvestDate", "PlantingDate" },
+                values: new object[] { new DateTime(2024, 12, 2, 9, 48, 56, 692, DateTimeKind.Local).AddTicks(7908), new DateTime(2024, 11, 22, 9, 48, 56, 692, DateTimeKind.Local).AddTicks(7905) });
+
+            migrationBuilder.UpdateData(
+                table: "GrowthHistory",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "HarvestDate", "PlantingDate" },
+                values: new object[] { new DateTime(2024, 12, 2, 9, 48, 56, 692, DateTimeKind.Local).AddTicks(7915), new DateTime(2024, 11, 22, 9, 48, 56, 692, DateTimeKind.Local).AddTicks(7912) });
+
+            migrationBuilder.UpdateData(
+                table: "HarvestTracking",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "HarvestDate",
+                value: new DateTime(2024, 12, 2, 9, 48, 56, 692, DateTimeKind.Local).AddTicks(7931));
+
+            migrationBuilder.UpdateData(
+                table: "HarvestTracking",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "HarvestDate",
+                value: new DateTime(2024, 12, 2, 9, 48, 56, 692, DateTimeKind.Local).AddTicks(7935));
+
+            migrationBuilder.UpdateData(
+                table: "PlantingSchedule",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PlantingDate",
+                value: new DateTime(2024, 10, 23, 9, 48, 56, 692, DateTimeKind.Local).AddTicks(7843));
+
+            migrationBuilder.UpdateData(
+                table: "PlantingSchedule",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PlantingDate",
+                value: new DateTime(2024, 9, 23, 9, 48, 56, 692, DateTimeKind.Local).AddTicks(7888));
+        }
+    }
+}
---
+++ DWA-AU24-Lab2-Group-11/Services/PlantingScheduleService.cs
@@ -0,0 +1,24 @@
+﻿using DWA_AU24_Lab2_Group_11.Models;
+
+namespace DWA_AU24_Lab2_Group_11.Services
+{
+    public class PlantingScheduleService
+    {
+        public DateTime? CalculateOptimalPlantingDate(Crop crop, WeatherData[] weatherForecast)
+        {
+          
+            crop.SetOptimalTemperatureRange();
+
+            foreach (var day in weatherForecast)
+            {
+                if (day.Temperature >= crop.OptimalTemperatureMin &&
+                    day.Temperature <= crop.OptimalTemperatureMax)
+                {
+                    return day.Date;  
+                }
+            }
+
+            return null;  
+        }
+    }
+}
